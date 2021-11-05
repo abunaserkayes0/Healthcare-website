8f5e75529aa817c2cc9b071144f93ba4a62097e1
abunaserkayes0 <abunaserkayes0.gamil.com>
2021-11-05 22:30:00 +0600

    massage

---
+++ src/Component/Navigation/Navigation.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import './Navigation.css';
+const Navigation = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand href="#home">Special Hospital</Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end text-center">
+                        <Nav>
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        </Nav>
+                    <Navbar.Text>
+                        Signed in as: <a href="#login">Mark Otto</a>
+                    </Navbar.Text>
+                    </Navbar.Collapse>
+                </Container>
+           </Navbar>
+        </div>
+    );
+};
+
+export default Navigation;
