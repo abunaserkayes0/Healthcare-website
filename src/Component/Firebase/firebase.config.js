d72d5d5092c1636e3a46c879246d4fe1a8fd685e
abunaserkayes0 <abunaserkayes0.gamil.com>
2021-11-06 14:26:57 +0600

    massage

---
+++ src/Component/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCOV0H0fJtY9r2mO8jEUn2cwUKJwIH45Lw",
+    authDomain: "specialized-hospital-758e3.firebaseapp.com",
+    projectId: "specialized-hospital-758e3",
+    storageBucket: "specialized-hospital-758e3.appspot.com",
+    messagingSenderId: "621090811423",
+    appId: "1:621090811423:web:3810a0207436cfa616ef2a"
+};
+ 
+export default firebaseConfig;
